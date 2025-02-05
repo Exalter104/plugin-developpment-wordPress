1804e746ce977b0db8bf583d7a9119d1791b734a
unknown <sananjani104@gmail.com>
2025-02-05 16:41:45 +0500

    Php Dynmic varibles for wordpress plugins

--- php-html/php-with-html.php
+++ php-html/php-with-html.php
@@ -1,5 +1,6 @@
 
 <?php
+
 // PHP With HTML
 
 echo '<h1 style="color:green"> I am the Ummathi of Hazrat Muhammad (SAW).</h1>';
@@ -8,6 +9,8 @@ echo("<h2 style= 'color: red'> The Most Supreme Zaat in Universe is  <h1>$my_Rab
 ?>
 
 <?php
-// Dynamic Color and Htm
-
+$h1_color="red";
+// Dynamic Color and Html
+ 
 ?>
+<h1 style= "color: <?php echo $h1_color; ?>"><?php echo ' I am dynamic text colors'; ?> </h1>;
