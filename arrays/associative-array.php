071819673cf0f202700a5890d74cb84821977d09
exalter104 <sananjani104@gmail.com>
2025-02-13 18:29:21 +0500

    Associative array :Find job title

--- arrays/associative-array.php
+++ arrays/associative-array.php
@@ -8,9 +8,10 @@
 </head>
 
 <body>
-    <form method="">
-        <label>Enter a Country</label><br>
-        <input type="text" name="country" required>
+    <form method="post">
+        <h4>Associative Array to Find Job Title</h4>
+        <label>Enter a Exalter</label><br>
+        <input type="text" name="roles" required>
         <input type="submit" value="Submit">
 
 
@@ -21,19 +22,16 @@
 <?php
 
 
-echo "Associative Array";
+
 //Associative Array : a array which having a key-value pair
 
 $exalters = array("Ikram" => "CEO", "Saqib" => "Acting CTO", "Sheraz" => "Adminstrative", "Sughra" => "Project Manager", "Sarran" => "AI Team Lead", "Mueen" => "Mobile Team", "Bilal" => "Design Team Lead",);
-// $exalters["Sughra"] = "Ayesha";
-// array_pop($exalters);
-// array_shift($exalters);
-// $keys = array_keys($exalters);
-// $values = array_values($exalters);
-$exalter = array_flip($exalters);
+
+$jobTitle = $exalters[$_POST["roles"]];
+echo "Your job title is  : {$jobTitle}";
 
 
-foreach ($exalters as $key => $value) {
-    echo "<br>";
-    echo "{$key} = {$value}";
-}
+
+
+
+?>
